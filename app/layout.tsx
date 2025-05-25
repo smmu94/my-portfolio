a9281a82b4cb5b40abd6cfff98160c11555bfc40
smmu94 <stephaniemmu@gmail.com>
2025-05-25 21:40:59 +0200

    Update home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,19 @@
-import './styles/global.css';
-import { press_start_2p } from './utils/fonts';
+import { ReactNode } from "react";
+import Navbar from "./components/navbar";
+import TopActionsBar from "./components/topActionsBar";
+import "./styles/global.css";
+import { press_start_2p } from "./utils/fonts";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${press_start_2p.className} antialiased`}
-      >
-        <div className="background-global">
-          {/* <Navbar /> */}
-          <main className="relative z-20 flex-grow p-8 min-h-screen">
-            {children}
+      <body className={`${press_start_2p.className} antialiased`}>
+        <div className="background-global bg-hero-gradient flex flex-col h-screen w-screen overflow-hidden">
+          <TopActionsBar />
+          <main className="flex-1 overflow-auto pb-36 md:pb-48">
+            <div className="h-full max-h-full overflow-auto">{children}</div>
           </main>
-          {/* <Footer /> */}
+          <Navbar />
         </div>
       </body>
     </html>
