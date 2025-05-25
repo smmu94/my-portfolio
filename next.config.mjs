4f0ef1a81b8ba3c0689354cf9c59124b4ab17638
smmu94 <stephaniemmu@gmail.com>
2025-05-25 21:38:52 +0200

    Add config to add svg icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-// next.config.mjs
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
