302fc3cc70842faeac5daf755bebc0468230f6d5
smmu94 <stephaniemmu@gmail.com>
2025-05-26 18:34:33 +0200

    Add experience page

---
+++ app/experience/constants.ts
@@ -0,0 +1,67 @@
+export const experienceData = [
+  {
+    role: "Frontend Developer",
+    company: "Simetrik",
+    location: "Remote (Bogotá, Colombia)",
+    date: "Aug 2022 – Aug 2024",
+    summary:
+      "Developed and maintained React-based accounting modules, improving form reliability and reducing errors by 25%. Enhanced testing practices and contributed to a shared component library, ensuring consistent UI and stable dependencies across teams.",
+    tech: [
+      "React",
+      "TypeScript",
+      "React Hook Form",
+      "Yup",
+      "Jest",
+      "RTL",
+      "Git",
+    ],
+  },
+  {
+    role: "Frontend Developer (Freelance)",
+    company: "Remote (Bogotá, Colombia)",
+    location: "",
+    date: "Feb 2025",
+    summary:
+      "Built a responsive Next.js mobile phone catalog app featuring dynamic filtering, modular architecture, and a fully functional shopping cart with persistent state. Ensured a seamless user experience across devices with comprehensive unit testing.",
+    tech: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Sass",
+      "Node.js",
+      "React Context API",
+      "Jest",
+    ],
+  },
+];
+
+export const skills = [
+  "React",
+  "Next.js",
+  "JavaScript",
+  "TypeScript",
+  "Redux",
+  "React Hooks",
+  "Context API",
+  "HTML",
+  "CSS",
+  "Sass",
+  "Tailwind CSS",
+  "Jest",
+  "React Testing Library",
+  "Git",
+  "GitHub",
+  "Bitbucket",
+  "Storybook",
+  "Figma",
+  "Notion",
+  "Trello",
+  "Slack",
+  "VS Code",
+  "Agile / Scrum",
+  "Spanish (Native)",
+  "English (Professional)",
+];
+
+export const resumeLink =
+  "https://drive.google.com/file/d/1vwqx0otMHjFHXMZjXgnhsZ6yYbdR4tsR/view?usp=sharing";
